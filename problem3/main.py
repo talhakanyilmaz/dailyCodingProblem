800df1d39a17f105ee72481386b6b4141a912552
talhakanyilmaz <talhakanyilmaz18@gmail.com>
2019-05-11 20:06:52 +0300

    add deserialize(s)

--- problem3/main.py
+++ problem3/main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 class Node:
     def __init__(self, val, left = None, right = None):
         self.val = val
@@ -7,4 +9,23 @@ class Node:
 def serialize(root):
     if(root == None):
         return ''
-    return root.val +  serialize(root.left) + serialize(root.right) 
+    return root.val + ':' + serialize(root.left) + serialize(root.right)
+
+def deserialize(s):
+    string = s.split(':')
+    node = None
+    if(s != ''): node = Node('root')
+    for str in string:
+        if(str == ''):
+            continue
+        temp = node
+        nodes = str.split('.')
+        for value in nodes:
+            if(value == 'left'):
+                temp.left = Node(value)
+                temp = temp.left
+            elif(value == 'right'):
+                temp.right = Node(value)
+                temp = temp.right
+        temp.val = str
+    return node
